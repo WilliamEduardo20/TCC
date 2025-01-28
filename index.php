f2462d932f102702aaeb3254deabaa68b48745a6
WilliamEduardo20 <williameduardominorelli@gmail.com>
2025-01-28 18:17:20 -0300

    18:16 28/01/2025
    Começo da barra de navegação mobile

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header></header>
+    <main></main>
+    <footer></footer>
+
+    <aside>
+        <section class="barraLateral">
+
+        </section>
+        <section class="barraVertical">
+            <div class="navBar">
+                <ul>
+                    <li>
+                        <i class="icon bi bi-circle-half"></i>
+                    </li>
+                    <li>
+                        <ul>
+                            <li>
+                                <i class="icon bi bi-book-half"></i>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li>
+                                        <i class="icon bi bi-house"></i>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <i class="icon bi bi-gear"></i>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <i class="icon bi bi-person-circle"></i>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </aside>
+</body>
+</html>
